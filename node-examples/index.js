7db11aaa00d904e8b088e3c32f4e04f299b6183c
Shajee98 <45584169+Shajee98@users.noreply.github.com>
2020-10-11 21:48:46 +0500

    Final Commit

---
+++ node-examples/index.js
@@ -0,0 +1,23 @@
+
+var rect = require('./rectangle');
+
+function solveRect(l,b)
+{
+    console.log("Solving or rectangle with l = " + l  + " and b = " + b);
+    rect(l,b,(err, rectangle) =>
+    {
+        if (err)
+        {
+            console.log("ERROR: ", err.message);
+
+        }
+        else {
+          console.log("The area of the rectangle of dimensions of l = "
+           + l + " and b = " + b + " is "  + rectangle.area());
+           console.log("The perimeter of the rectangle of dimensions of l = "
+           + l + " and b = " + b + " is "  + rectangle.perimeter());
+        }
+    });
+}
+
+solveRect(-3,4);
